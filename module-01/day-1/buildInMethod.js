71c1bba5091d52f5de1401c3ba535c54348db8ac
Hendri Gunawan <hendrixia54@gmail.com>
2025-09-02 11:25:39 +0700

    day1-day3

---
+++ module-01/day-1/buildInMethod.js
@@ -0,0 +1,20 @@
+// NUMBER
+
+let angka = 5.152312;
+console.log(angka.toFixed(2));
+// angka 2 dalam kurung menyatakan ambil 2 angka dibelakang koma
+
+
+// STRING
+
+let text = "hello, budi";
+console.log(text.toUpperCase());
+let textAngka = "5";
+console.log(Number(textAngka));
+console.log("HELLO, BUDI".toLowerCase());
+
+let num = "5.25345";
+num = Number(num);
+num = num.toFixed(1);
+
+console.log(num);
---
+++ module-01/day-1/datatype.js
@@ -0,0 +1,10 @@
+// tipe data string (text)
+var text = "hello";
+console.log("5 + 5")
+
+// tipe data number (angka)
+var number = 50000;
+console.log(number);
+
+// tipe data boolean (true / false)
+var boolean = false;
